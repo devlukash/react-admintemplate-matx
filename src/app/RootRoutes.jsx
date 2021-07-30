1fca075e8771e477e648d14deecb8af256d9b91d
devlukash <dev.lukash1025@gmail.com>
2021-07-30 03:29:24 -0500

    Deploy

---
+++ src/app/RootRoutes.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+
+import dashboardRoutes from './views/dashboard/DashboardRoutes'
+import utilitiesRoutes from './views/utilities/UtilitiesRoutes'
+
+import materialRoutes from './views/material-kit/MaterialRoutes'
+import chartsRoute from './views/charts/ChartsRoute'
+import dragAndDropRoute from './views/Drag&Drop/DragAndDropRoute'
+
+import formsRoutes from './views/forms/FormsRoutes'
+import mapRoutes from './views/map/MapRoutes'
+
+
+const redirectRoute = [
+    {
+        path: '/',
+        exact: true,
+        component: () => <Redirect to="/dashboard/default" />,
+    },
+]
+
+const errorRoute = [
+    {
+        component: () => <Redirect to="/session/404" />,
+    },
+]
+
+const routes = [
+    ...dashboardRoutes,
+    ...materialRoutes,
+    ...utilitiesRoutes,
+    ...chartsRoute,
+    ...dragAndDropRoute,
+    ...formsRoutes,
+    ...mapRoutes,
+    ...redirectRoute,
+    ...errorRoute,
+]
+
+export default routes
